54b5d080574f0b570e3ac69314e40d4aa14ba20a
Rysted <rysted.dev@gmail.com>
2026-05-05 17:12:03 -0300

    Update blacklist to comment out user ID
    Commented out the user ID in the blacklist.

--- black_list.lua
+++ black_list.lua
@@ -2,7 +2,7 @@ local Players = game:GetService("Players")
 local localPlayer = Players.LocalPlayer
 
 local BLACKLIST = {
-	[1545207970] = true,
+	-- [1545207970] = true,
 }
 
 local fakeban = "You have been permanently banned. \n Rysted"
